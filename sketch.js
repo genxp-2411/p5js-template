70905d2cffb7e0130255fae1fef858b4cb97def1
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-12-22 21:08:33 -0500

    fix background for trails

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
 }
 
 function draw() {
-  background(0);
   fill(20, 120, 220, 10);
-  noStroke();
   rect(0,0,width,height);
   fill(255);
   ellipse(mouseX, mouseY, 50);
