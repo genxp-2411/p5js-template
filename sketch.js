94be2723684462d2494cd31948e6f8b1a62a833f
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-12-22 21:07:08 -0500

    add ellipse with trails

--- sketch.js
+++ sketch.js
@@ -3,5 +3,10 @@ function setup() {
 }
 
 function draw() {
-  background(20, 120, 220);
+  background(0);
+  fill(20, 120, 220, 10);
+  noStroke();
+  rect(0,0,width,height);
+  fill(255);
+  ellipse(mouseX, mouseY, 50);
 }
